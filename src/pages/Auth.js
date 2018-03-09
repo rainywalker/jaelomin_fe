7a18cbd4be7cdcb6ad0fdd79cbcf7ca86c6e36bb
jaelomin <jaelomin@urbanbase.com>
2018-03-09 16:10:28 +0900

    로그인 틀 작업

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as baseActions from 'redux/modules/base';
+import { AuthWrapper } from 'components/Auth';
+import { Route } from 'react-router-dom';
+import { Login, Register } from 'containers/Auth';
 
 
 class Auth extends Component {
@@ -18,9 +21,10 @@ class Auth extends Component {
 
     render() {
         return (
-            <div>
-                Auth
-            </div>
+            <AuthWrapper>
+                <Route path="/auth/login" component={Login}/>
+                <Route path="/auth/register" component={Register}/>
+            </AuthWrapper>
         );
     }
 }
