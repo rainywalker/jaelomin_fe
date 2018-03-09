9b0244813b1e4d5dfcf1e3d8dd259c8bd4edfd4c
jaelomin <jaelomin@urbanbase.com>
2018-03-09 14:53:44 +0900

    헤더 구성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import {Route} from 'react-router-dom';
 import {Home, Auth} from 'pages';
+import HeaderContainer from 'containers/Base/HeaderContainer';
+
 
 class App extends Component {
     render() {
         return (
             <div>
+                <HeaderContainer/>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/auth' component={Auth} />
             </div>
